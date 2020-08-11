72bc00b0824d3bdd0e1c8a89d29b0f41a2aae945
ZhangHui <hui.zhang05@hand-china.com>
2020-08-11 14:19:51 +0800

    fourth change

--- foundation/index.js
+++ foundation/index.js
@@ -1,3 +1,4 @@
 // this is index of foundation
 // second change of foundation's index
 // third change of foundation's index
+// fourth change
--- train/index.js
+++ train/index.js
@@ -1,3 +1,4 @@
 // this is index of train
 // second change of train's index
 // third change of train's index
+// fourth change
